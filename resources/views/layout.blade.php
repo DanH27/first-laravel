e81c714e54c8e4ead1388cba7bc370d91bf8282b
Dexter2772 <31225973+Dexter2772@users.noreply.github.com>
2019-02-20 11:56:54 -0500

    Add layout template

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+  <title></title>
+</head>
+<body>
+  @yield('content')
+
+
+  <ul>
+    <li>
+      <li>
+        <a href="/about">About us</a>
+      </li>
+      <li>
+        <a href="/contact">Contact</a>
+      </li>
+    </li>
+  </ul>
+</body>
+
+
+
+
+
+
+
+
+
+
+
+
+</html>
